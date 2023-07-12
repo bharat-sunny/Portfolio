ab5dfb77eddcd450918a2fc21a6d62c5f158cc5c
bharat <bharat.t@northeastern.edu>
2023-07-13 03:13:06 +0530

    Landing Page done

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,15 +1,17 @@
-import Image from 'next/image'
-import Header from '../components/Header'
+import Image from 'next/image';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <div>
-        <h1 className='p-10 text-red-500'>
-          Lets build our first portfolio
-        </h1>
-      </div>
-      
-    </main>
-  )
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+    overflow-scroll z-0'>
+      <main>
+        <Header />
+        <section id= "hero" className='snap-center'>
+          <Hero />
+        </section>
+      </main>
+    </div>
+  );
 }
