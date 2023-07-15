c142a58c210598d60951c6a832321e36adece259
bharat <bharat.t@northeastern.edu>
2023-07-16 01:56:38 +0530

    Projects section done

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import Experience from '@/components/Experience';
 import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
 
 
 export default function Home() {
@@ -11,7 +12,9 @@ export default function Home() {
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
     overflow-scroll z-0'>
       <main>
-        <Header />
+        
+          <Header />
+
         
         <section id= "hero" className='snap-start'>
           <Hero />
@@ -27,7 +30,10 @@ export default function Home() {
 
         <section id="skills" className='snap-center'>
           <Skills />
+        </section>
 
+        <section id="projects" className='snap-center'>
+          <Projects />
         </section>
 
       </main>
--- portfolio/components/About.tsx
+++ portfolio/components/About.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import Profpic from './images/b2c1857e-7f39-4cbf-9b60-e24edc730c63.jpeg';
-import Image from "next/image";
 import { motion } from 'framer-motion';
 
 type Props = {}
@@ -17,7 +16,7 @@ function About({}: Props) {
     className='flex flex-col relative h-screen text-center md:text-left
     md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center
     '>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+        <h3 className='absolute top-14  uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
         
         <motion.img 
         initial={{
