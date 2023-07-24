df78b3032e7d78b64c6d1e1a71d34fdb46b73323
bharat <bharat.t@northeastern.edu>
2023-07-24 12:52:14 +0530

    Vercel Link added

--- portfolio/utils/fetchPageInfo.ts
+++ portfolio/utils/fetchPageInfo.ts
@@ -2,7 +2,7 @@ import { PageInfo } from "@/typings";
 
 
 export const fetchPageInfo= async() =>{
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
+    const res = await fetch(`https://portfolio-st9x.vercel.app/api/getPageInfo`);
     
     const data = await res.json()
     const pageInfo: PageInfo[] = data.pageInfo;
